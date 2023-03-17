8ddd50ef353f47812ecde3adb1428119195d374b
Mike Patay <91127463+itsmikupaths@users.noreply.github.com>
2023-03-17 15:39:49 +0800

    data option and interponation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <script>
-
+  export default {
+    data: () => {
+      return {
+        title: "Todo Reminder",
+        message: "Hello Vue App"
+      }
+    }
+  };
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <h2>Todo Reminder</h2>
+      <h2>{{ title }}</h2>
     </div>
   </header>
 
   <main>
-    Hello Vue App
+    {{ message }}
   </main>
 </template>
 
