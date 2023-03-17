a356f02bd9e20a729642c5c35283b672d9ed3c32
Mike Patay <91127463+itsmikupaths@users.noreply.github.com>
2023-03-17 18:42:37 +0800

    component and props

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script>
+  import Todo from './components/todo.vue';
+
   export default {
+    components: {
+      Todo
+    },
     data: () => {
       return {
         name: "Todo Reminder",
@@ -32,13 +37,7 @@
   </header>
 
   <main>
-    <article v-for="(todo, index) in todos" :key="index">
-      <div>
-        <h3>{{ todo.title }}</h3>
-        <i v-if="todo.done" class="check-icon"></i>
-      </div>
-      <p>{{ todo.description }}</p>
-    </article>
+    <Todo v-for="(todo, index) in todos" :todo="todo" :index="index"></Todo>
   </main>
 </template>
 
@@ -61,37 +60,5 @@
     grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
     grid-gap: 8px;
     width: 100%;
-  }
-
-  /* article */
-  article {
-    padding: 10px 5px;
-    border-radius: 5px;
-    border: 1px solid rgb(224, 224, 224);
-    cursor: pointer;
-    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.2),
-                0 3px 5px rgba(0, 0, 0, 0.2);
-  }
-  article div {
-    position: relative;
-  }
-  article div h3 {
-    border-bottom: 1px solid grey;
-    font-size: 22px;
-  }
-  article p {
-    margin-top: 5px;
-  }
-  i.check-icon {
-    border-right: 4px solid green;
-    border-bottom: 4px solid green;
-    position: absolute;
-    top: 0;
-    right: 10px;
-    width: 7px;
-    height: 14px;
-    transform: translateY (50%);
-    rotate: 45deg;
-    transform-origin: left bottom;
   }
 </style>
---
+++ src/components/todo.vue
@@ -0,0 +1,58 @@
+<script>
+  export default {
+    props: {
+      todo: {
+        type: Object,
+        required: true,
+      },
+      index: {
+        type: Number,
+        required: true,
+      }
+    }
+  }
+</script>
+
+<template>
+  <article>
+      <div>
+        <h3>{{ todo.title }}</h3>
+        <i v-if="todo.done" class="check-icon"></i>
+      </div>
+      <p>{{ todo.description }}</p>
+    </article>
+</template>
+
+<style scoped>
+/* article */
+  article {
+    padding: 10px 5px;
+    border-radius: 5px;
+    border: 1px solid rgb(224, 224, 224);
+    cursor: pointer;
+    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.2),
+                0 3px 5px rgba(0, 0, 0, 0.2);
+  }
+  article div {
+    position: relative;
+  }
+  article div h3 {
+    border-bottom: 1px solid grey;
+    font-size: 22px;
+  }
+  article p {
+    margin-top: 5px;
+  }
+  i.check-icon {
+    border-right: 4px solid green;
+    border-bottom: 4px solid green;
+    position: absolute;
+    top: 0;
+    right: 10px;
+    width: 7px;
+    height: 14px;
+    transform: translateY (50%);
+    rotate: 45deg;
+    transform-origin: left bottom;
+  }
+</style>
