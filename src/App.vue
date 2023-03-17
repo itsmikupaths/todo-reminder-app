efe895a57e444d344f86670f581e226be3549528
Mike Patay <91127463+itsmikupaths@users.noreply.github.com>
2023-03-17 18:12:01 +0800

    props and component start

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,12 @@
       return {
         name: "Todo Reminder",
         count: 0,
-        message: "Hello Vue App",
         logoPath: "./src/assets/images/todo-reminder-logo.png",
-        showList: true,
-        todos: ["Code", "Eat", "Sleep", "Repeat"],
+        todos: [
+          { title: "Meeting with John", description: "To discuss the next step on the project.", done: true },
+          { title: "Create pull request", description: "For code review and testing purposes.", done: false },
+          { title: "Early clock-out", description: "Personal errand.", done: false }
+        ]
       }
     },
     computed: {
@@ -30,13 +32,14 @@
   </header>
 
   <main>
-    <ul v-if="showList">
-      <li v-for="todo in todos">{{ todo }}</li>
-    </ul>
+    <article v-for="(todo, index) in todos" :key="index">
+      <div>
+        <h3>{{ todo.title }}</h3>
+        <i v-if="todo.done" class="check-icon"></i>
+      </div>
+      <p>{{ todo.description }}</p>
+    </article>
   </main>
-
-  <!-- v-on: directive -->
-  <button @click="toggleTodos">Toggle Todos</button>
 </template>
 
 <style scoped>
@@ -54,9 +57,41 @@
   }
   main {
     margin-top: 10px;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(220px, 1fr));
+    grid-gap: 8px;
+    width: 100%;
   }
-  ul {
-    list-style-type: none;
-    padding: 0;
+
+  /* article */
+  article {
+    padding: 10px 5px;
+    border-radius: 5px;
+    border: 1px solid rgb(224, 224, 224);
+    cursor: pointer;
+    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.2),
+                0 3px 5px rgba(0, 0, 0, 0.2);
+  }
+  article div {
+    position: relative;
+  }
+  article div h3 {
+    border-bottom: 1px solid grey;
+    font-size: 22px;
+  }
+  article p {
+    margin-top: 5px;
+  }
+  i.check-icon {
+    border-right: 4px solid green;
+    border-bottom: 4px solid green;
+    position: absolute;
+    top: 0;
+    right: 10px;
+    width: 7px;
+    height: 14px;
+    transform: translateY (50%);
+    rotate: 45deg;
+    transform-origin: left bottom;
   }
 </style>
